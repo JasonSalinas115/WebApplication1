5849d6195612c776da43257a3a31687ec3161dac
jasonsalinas2 <jasonsalinas2@LEM231-09>
2025-03-24 18:35:08 -0700

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,36 @@
+/*
+ * Author: Jason Salinas
+ * Course: COM{-))3B
+ * Faculty: Jonathan Cruz
+ * Purose: Working on Middleware
+ * 
+ */
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseWelcomePage("/welcome");
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
